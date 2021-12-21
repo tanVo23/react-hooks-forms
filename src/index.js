f7bd8ea1486b8b2a24fc87fa84d376c8192bfe69
Tan <tan10tvo@gmail.com>
2021-12-20 21:46:23 -0700

    commit tree 6f859d8b6e5d5edb84929b6a2b045893fae989d8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
 
 ReactDOM.render(
   <Form />,
+  <ParentComponent/>,
   document.getElementById('root')
 );
